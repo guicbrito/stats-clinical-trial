1df06a248429d36e66f250dd1bf09dea194ac9c6
guicbrito <8864265+guicbrito@users.noreply.github.com>
2023-03-31 18:01:50 -0300

    Working fine

--- data-analysis-script.r
+++ data-analysis-script.r
@@ -1,9 +1,8 @@
 # Statistical analysis
 
-print("Essa análise requer os seguintes pacotes instalados no R:
-	readxl, writexl, tidyverse, broom, openxlsx")
+print("Essa análise requer os seguintes pacotes instalados: rio, tidyverse, broom, stats")
 
-# Load required packages
+## Load required packages
 library(tidyverse)
 library(rio)
 library(broom)
@@ -13,77 +12,108 @@ if (!dir.exists("output")) {
     dir.create("output")
 }
 
-# Read input data
-data <- import("input/data.xlsx", readxl = FALSE, na.strings = "NA")
+## Read input data
+data <- import("input/data.xlsx", setclass = "tibble", na = "NA")
 
-target_vars_stats <- names(read_csv("input/target-variables-stats.csv"))
-target_vars_freq <- names(read_csv("input/target-variables-freq.csv"))
-target_vars_group_diff <- names(read_csv("input/target-variables-group-diff.csv"))
-target_vars_cor <- names(read_csv("input/target-variables-cor.csv"))
+target_vars_stats <- names(import(
+    "input/target-variables-descriptive-stats.csv",
+    setclass = "tibble"
+))
+target_vars_frequencies <- names(import(
+    "input/target-variables-frequecies.csv",
+    setclass = "tibble"
+))
+target_vars_group_differences <- names(import(
+    "input/target-variables-group-differences.csv",
+    setclass = "tibble"
+))
+target_vars_correlations <- names(import(
+    "input/target-variables-corrolations.csv",
+    setclass = "tibble"
+))
 
-# Descriptive statistics
-calc_descriptive_stats <- function(data, target_vars_stats) {
+colnames(data) <- iconv(colnames(data),
+    from = "UTF-8", to = "ASCII//TRANSLIT"
+)
+
+target_vars_stats <- iconv(target_vars_stats,
+    from = "UTF-8", to = "ASCII//TRANSLIT"
+)
+target_vars_frequencies <- iconv(target_vars_frequencies,
+    from = "UTF-8", to = "ASCII//TRANSLIT"
+)
+target_vars_group_differences <- iconv(target_vars_group_differences,
+    from = "UTF-8", to = "ASCII//TRANSLIT"
+)
+target_vars_correlations <- iconv(target_vars_correlations,
+    from = "UTF-8", to = "ASCII//TRANSLIT"
+)
+
+data_with_age_category <- data %>%
+    mutate(age_category = ifelse(Idade <= 20, "20_or_below", "above_20"))
+
+## Descriptive statistics
+calculate_descriptive_stats <- function(data, target_vars_stats) {
     data %>%
         select(all_of(target_vars_stats)) %>%
-        gather(variable, value) %>%
-        group_by(variable) %>%
+        gather(Variable, Value) %>%
+        group_by(Variable) %>%
         summarise(
-            mean = mean(value, na.rm = TRUE),
-            median = median(value, na.rm = TRUE),
-            sd = sd(value, na.rm = TRUE),
-            iqr = IQR(value, na.rm = TRUE)
+            mean = mean(Value, na.rm = TRUE),
+            median = median(Value, na.rm = TRUE),
+            sd = sd(Value, na.rm = TRUE),
+            iqr = IQR(Value, na.rm = TRUE)
         )
 }
 
-descriptive_stats <- calc_descriptive_stats(data, target_vars_stats)
-write.csv(descriptive_stats, "output/descriptive-stats.csv")
-
-# Frequency calculations
-calc_frequencies <- function(data, vars) {
+## Frequency calculations
+calc_frequencies <- function(data, target_vars_frequencies) {
     data %>%
-        select(Grupo, any_of(vars)) %>%
-        gather(variable, value, -Grupo) %>%
-        group_by(Grupo, variable, value) %>%
+        select(Grupo, all_of(target_vars_frequencies)) %>%
+        gather(Variable, Value, -Grupo) %>%
+        group_by(Grupo, Variable, Value) %>%
         tally() %>%
-        mutate(freq_rel = n / sum(n)) %>%
+        mutate(Frequencia_relativa = n / sum(n)) %>%
         ungroup() %>%
-        select(variable, value, Grupo, n, freq_rel)
+        select(Variable, Value, Grupo, n, Frequencia_relativa)
 }
 
-frequencies <- calc_frequencies(data, target_vars_freq$variable)
-write.csv(frequencies, "output/var-freq.csv")
-
-# Group differences
-calc_group_diff <- function(data, vars) {
+## Group differences
+calc_group_differences <- function(data, vars) {
     # Implement the appropriate significance tests and confidence intervals
 }
 
-group_diff <- calc_group_diff(data, target_vars_group_diff$variable)
-write.csv(group_diff, "output/group-diff.csv")
 
-# Correlation matrix
-calc_correlations <- function(data, vars) {
+## Correlation matrix
+calc_correlations <- function(data, target_vars_correlations) {
     data %>%
-        select(any_of(vars)) %>%
+        select(all_of(target_vars_correlations)) %>%
         cor(use = "pairwise.complete.obs") %>%
         as.data.frame() %>%
-        rownames_to_column(var = "variable1") %>%
-        gather(variable2, correlation, -variable1)
+        rownames_to_column(var = "Variable1") %>%
+        gather(Variable2, correlation, -Variable1)
 }
 
-correlations <- calc_correlations(data, target_vars_cor$variable)
-write.csv(correlations, "output/descriptive-cor.csv")
 
-# Categorical age variable
-data_with_age_cat <- data %>%
-    mutate(age_cat = ifelse(Idade <= 20, "20_or_below", "above_20"))
-
-write.csv(data_with_age_cat, "input/data-with-age-cat.csv")
-
-# Age group differences
+## Age group differences
 calc_age_group_diff <- function(data, vars) {
     # Implement the appropriate significance tests and confidence intervals
 }
 
-age_group_diff <- calc_age_group_diff(data_with_age_cat, target_vars_age_diff$variable)
-write.csv(age_group_diff, "output/group-diff-age-cat.csv")
+# Run analysis
+age_group_diff <- calc_age_group_diff(data_with_age_category, target_vars_age_diff)
+descriptive_stats <- calculate_descriptive_stats(data, target_vars_stats)
+frequencies <- calc_frequencies(data, target_vars_frequencies)
+group_diff <- calc_group_differences(data, target_vars_group_differences)
+correlations <- calc_correlations(data, target_vars_correlations)
+DNA_corrigido15x_correlations <- correlations %>%
+    filter(Variable1 == "DNA_corrigido15x")
+
+# Export results
+write_csv(age_group_diff, "output/sub20-vs-above20-differences.csv")
+write_csv(data_with_age_category, "input/data-with-age-category.csv")
+write_csv(descriptive_stats, "output/descriptive-stats.csv")
+write_csv(frequencies, "output/variables-frequency.csv")
+write_csv(group_diff, "output/group-differences.csv")
+write_csv(DNA_corrigido15x_correlations, "output/DNA_corrigido15x-correlations.csv")
+write_csv(correlations, "output/variables-correlations.csv")
