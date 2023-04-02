73fe270ba7fafbc437cde6f6cf0fc03b9b31cf1e
guicbrito <8864265+guicbrito@users.noreply.github.com>
2023-04-02 09:16:50 -0300

    kable

--- data-analysis-script.r
+++ data-analysis-script.r
@@ -2,6 +2,7 @@ library(tidyverse)
 library(rio)
 library(broom)
 library(openxlsx)
+library(kableExtra)
 
 p.value <- 0.05
 
@@ -33,92 +34,91 @@ data <- import("input/data.xlsx", setclass = "tibble", na = "NA") %>%
     setNames(iconv(colnames(.), from = "UTF-8", to = "ASCII//TRANSLIT")) %>%
     mutate(Categoria_Idade = if_else(Idade <= 20, "20_ou_menos", "acima_de_20"))
 
+
 shapiro <- function(data, vars) {
     data %>%
         select(all_of(vars)) %>%
-        gather(Variable, Value) %>%
-        group_by(Variable) %>%
+        gather(Variável, Valor) %>%
+        group_by(Variável) %>%
         summarise(
             n = n(),
-            shapiro_w = shapiro.test(Value)$statistic,
-            shapiro_p = shapiro.test(Value)$p.value,
-            normality = if_else(shapiro.test(Value)$p.value > p.value, "parametric", "non-parametric")
+            shapiro_w = shapiro.test(Valor)$statistic,
+            shapiro_p = shapiro.test(Valor)$p.value,
+            normality = if_else(shapiro.test(Valor)$p.value > p.value,
+                "parametric", "non-parametric"
+            )
         )
 }
 
-calc_stats <- function(data, vars) {
+
+calc_stats <- function(data, vars, group_by_var = NULL, overall = TRUE) {
     data %>%
-        select(all_of(vars)) %>%
-        gather(Variable, Value) %>%
-        group_by(Variable) %>%
+        select(all_of(vars), if (!overall) all_of(group_by_var)) %>%
+        gather(Variável, Valor, -if (!overall) group_by_var) %>%
+        {
+            if (overall) {
+                group_by(., Variável)
+            } else {
+                group_by(., Variável, !!sym(group_by_var))
+            }
+        } %>%
         summarise(
             n = n(),
-            missing = sum(is.na(Value)),
-            normality = if_else(shapiro.test(Value)$p.value > p.value, "parametric", "non-parametric"),
-            mean = mean(Value, na.rm = TRUE),
-            median = median(Value, na.rm = TRUE),
-            sd = sd(Value, na.rm = TRUE),
-            se = sd(Value, na.rm = TRUE) / sqrt(length(Value)),
-            iqr = IQR(Value, na.rm = TRUE)
+            missing = sum(is.na(Valor)),
+            normality = if_else(shapiro.test(Valor)$p.value > p.value,
+                "parametric", "non-parametric"
+            ),
+            mean = mean(Valor, na.rm = TRUE),
+            median = median(Valor, na.rm = TRUE),
+            sd = sd(Valor, na.rm = TRUE),
+            se = sd(Valor, na.rm = TRUE) / sqrt(length(Valor)),
+            iqr = IQR(Valor, na.rm = TRUE)
         )
 }
+
 
 calc_freqs <- function(data, vars) {
     data %>%
         select(Grupo, all_of(vars)) %>%
-        gather(Variable, Value, -Grupo) %>%
-        count(Grupo, Variable, Value) %>%
-        group_by(Variable, Grupo) %>%
+        gather(Variável, Valor, -Grupo) %>%
+        count(Grupo, Variável, Valor) %>%
+        group_by(Variável, Grupo) %>%
         mutate(
             Freq_rel = n / sum(n),
             Freq_abs = n
         ) %>%
-        select(Variable, Value, Grupo, Freq_abs, Freq_rel)
+        select(Variável, Grupo, Valor, Freq_abs, Freq_rel) %>%
+        arrange(Variável, Grupo, Valor)
 }
+
 
 calc_diffs <- function(data, vars, normality, p.value, group_var) {
     map_df(vars, function(var) {
-        normality <- filter(normality, Variable == var)
+        normality <- filter(normality, Variável == var)
         is_normal <- normality$shapiro_p > p.value
-        test_result <- if (is_normal) t.test(data[[var]] ~ data[[group_var]]) else wilcox.test(data[[var]] ~ data[[group_var]], conf.int = TRUE)
+        test_result <- if (is_normal) {
+            t.test(data[[var]] ~ data[[group_var]])
+        } else {
+            wilcox.test(data[[var]] ~ data[[group_var]], conf.int = TRUE)
+        }
         effect_size <- if (is_normal) {
             cohen_d(data, var, group_var)
         } else {
             biserial(data, var, group_var)
         }
         tibble(
-            Variable = var,
-            Test = test_result$method,
+            Variável = var,
+            Teste = test_result$method,
             df = ifelse(is_normal, test_result$parameter, NA),
-            Statistic = test_result$statistic,
-            p.value = test_result$p.value,
-            Significance = ifelse(test_result$p.value < p.value, "*", "-"),
+            Estatística = test_result$statistic,
+            p_value = test_result$p.value,
+            Significância = ifelse(test_result$p.value < p.value, "*", "-"),
             Effect_Size = effect_size,
             CI_min = test_result$conf.int[1],
             CI_max = test_result$conf.int[2]
         )
-    })
-}
-
-calc_corrs <- function(data, vars, norms, p.value) {
-    combn(vars, 2, simplify = FALSE) %>%
-        map_df(function(pair) {
-            norms1 <- filter(norms, Variable == pair[1])
-            norms2 <- filter(norms, Variable == pair[2])
-            is_normal <- norms1$shapiro_p > p.value & norms2$shapiro_p > p.value
-            corr_result <- if (is_normal) cor.test(data[[pair[1]]], data[[pair[2]]], method = "pearson") else cor.test(data[[pair[1]]], data[[pair[2]]], method = "spearman")
-            tibble(
-                Variable1 = pair[1],
-                Variable2 = pair[2],
-                Test = corr_result$method,
-                df = corr_result$parameter,
-                Correlation = corr_result$estimate,
-                p.value = corr_result$p.value,
-                Significance = ifelse(corr_result$p.value < p.value, "*", "-"),
-                corr_CI_lower = corr_result$conf.int[1],
-                corr_CI_upper = corr_result$conf.int[2]
-            )
-        })
+    }) %>%
+        arrange(p_value)
 }
 
 cohen_d <- function(data, var, group_var) {
@@ -129,29 +129,84 @@ cohen_d <- function(data, var, group_var) {
         filter(data[[group_var]] == unique(data[[group_var]])[2]) %>%
         pull(var)
     mean_diff <- mean(group1, na.rm = TRUE) - mean(group2, na.rm = TRUE)
-    pooled_sd <- sqrt(((length(group1) - 1) * var(group1, na.rm = TRUE) + (length(group2) - 1) * var(group2, na.rm = TRUE)) / (length(group1) + length(group2) - 2))
+    pooled_sd <- sqrt(((length(group1) - 1) * var(group1, na.rm = TRUE) +
+        (length(group2) - 1) * var(group2, na.rm = TRUE))
+    / (length(group1) + length(group2) - 2))
     cohen_d <- mean_diff / pooled_sd
     return(cohen_d)
 }
 
 biserial <- function(data, var, group_var) {
-    data <- data %>% mutate(Rank = rank(data[[var]], na.last = "keep", ties.method = "average"))
+    data <- data %>% mutate(Rank = rank(data[[var]],
+        na.last = "keep",
+        ties.method = "average"
+    ))
     group1 <- data %>%
         filter(data[[group_var]] == unique(data[[group_var]])[1]) %>%
         pull(Rank)
     group2 <- data %>%
         filter(data[[group_var]] == unique(data[[group_var]])[2]) %>%
         pull(Rank)
-    rbc <- (sum(group1) / length(group1) - sum(group2) / length(group2)) / length(data[[var]])
+    rbc <- (sum(group1) / length(group1) - sum(group2) /
+        length(group2)) / length(data[[var]])
     return(rbc)
 }
 
+calc_corrs <- function(data, vars, norms, p.value) {
+    combn(vars, 2, simplify = FALSE) %>%
+        map_df(function(pair) {
+            norms1 <- filter(norms, Variável == pair[1])
+            norms2 <- filter(norms, Variável == pair[2])
+            is_normal <- norms1$shapiro_p > p.value & norms2$shapiro_p > p.value
+            corr_result <- if (is_normal) {
+                cor.test(data[[pair[1]]], data[[pair[2]]],
+                    method = "pearson"
+                )
+            } else {
+                cor.test(data[[pair[1]]],
+                    data[[pair[2]]],
+                    method = "spearman"
+                )
+            }
+            tibble(
+                Variável1 = pair[1],
+                Variável2 = pair[2],
+                Teste = corr_result$method,
+                df = corr_result$parameter,
+                Correlação = corr_result$estimate,
+                p_value = corr_result$p.value,
+                Significância = ifelse(corr_result$p.value < p.value, "*", "-"),
+                CI_lower = corr_result$conf.int[1],
+                CI_upper = corr_result$conf.int[2]
+            )
+        })
+}
+
+custom_kable <- function(table_data, ...) {
+    table_output <- kable(table_data, ...)
+    return(table_output)
+}
+
 normality <- shapiro(data, unlist(targets[names(targets) != "freqs"], use.names = FALSE) %>% unique())
+custom_kable(normality, digits = 3)
+
 stats <- calc_stats(data, unlist(targets$stats))
+a <- custom_kable(stats, digits = 1, align = "lcclrrrrr")
+
+stats <- calc_stats(data, unlist(targets$stats), "Grupo", overall = FALSE)
+b <- custom_kable(stats, digits = 1, align = "lcclrrrrr")
+
 freq <- calc_freqs(data, unlist(targets$freq))
+custom_kable(freq, digits = 2)
+
 diffs <- calc_diffs(data, unlist(targets$diffs), normality, p.value, "Grupo")
+custom_kable(diffs, digits = 2)
+
 age_diffs <- calc_diffs(data, unlist(targets$age_diffs), normality, p.value, "Categoria_Idade")
+custom_kable(age_diffs, digits = 2)
+
 corrs <- calc_corrs(data, unlist(targets$corrs), normality, p.value)
+custom_kable(corrs, digits = 2)
 
 # Save data
 data_list <- list(
@@ -161,7 +216,7 @@ data_list <- list(
     diffs,
     age_diffs,
     corrs,
-    filter(corrs, Variable1 == "DNA_corrigido15x")
+    filter(corrs, Variável1 == "DNA_corrigido15x")
 )
 
 data_file_names <- c(
@@ -186,4 +241,4 @@ walk2(data_list, data_file_names, ~ {
 })
 saveWorkbook(output_workbook, "output/results.xlsx", overwrite = TRUE)
 
-rm(list = Filter(function(x) !inherits(get(x), "tbl"), ls()))
+# rm(list = Filter(function(x) !inherits(get(x), "tbl"), ls()))
